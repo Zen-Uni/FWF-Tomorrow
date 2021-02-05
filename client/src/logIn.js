6e74fad49ff4e460e3d731538fce00b47c889d4a
jamond-x <x2592253600@163.com>
2021-02-05 12:35:08 +0800

    fix:Improve function

--- client/src/logIn.js
+++ client/src/logIn.js
@@ -1,29 +1,42 @@
 /**
  * @description 点击提交时或按回车触发check（）函数验证邮箱格式
- * @description 密码要求5-10位字母数字下划线
+ * @description 密码要求5-10位字母数字下划线 需要协调更改
+ * @param{string} className 类名
+ * @param{string} res 服务器返回信息 ----测试使用，需要删掉----
  * @author XXQ
  */
+function $(className){
+    return document.getElementsByClassName(className);
+}
 function showOutput(res){
     alert(`登录成功${JSON.stringify(res.data)}`)
 }
-const submit=document.getElementsByClassName("container-form-logInButton")[0];
+const submit=$("container-form-logInButton")[0];
 submit.addEventListener("click",check);
 function check(){
-    const password=document.getElementsByClassName('container-form-passwordInput')[0].value;
-    const email=document.getElementsByClassName('container-form-userInput')[0].value;
+    const password=$('container-form-passwordInput')[0].value;
+    const email=$('container-form-userInput')[0].value;
     if(email!=""&&password!=""){
         const needEmail=/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
         const needPassword=/^\w{5,10}$/;
         if(needEmail.test(email)&&needPassword.test(password)){
-            axios.post('http://jsonplaceholder.typicode.com/todos',{email:`${email}`,password:`${password}`})
-            //这里的axios需要替换
-            .then(res=>{
-                showOutput(res);
-                console.log(res);
-                window.location.href="/testHtml3.html"; //跳转至主页面
-            }
-            )
-            .catch(err=>{alert('登录失败，请重试！'); console.log(err);})
+                axios.post('http://60.205.230.224:8000/api/user/login',{email:`${email}`,password:`${password}`}) //{email:`${email}`,password:`${password}`}
+                .then(res=>{showOutput(res);console.log(res.data.code)
+                    if(res.data.code==0){   //code==0请求成功
+                    const _token=res.data.data.token;
+                        if(_token){
+                            window.localStorage.setItem("token",_token);//res.data.msg代为token信息，给出接口后修改！
+                            console.log(localStorage.getItem("token")+"-----------验证token储存！")
+                            window.location.href="/testHtml3.html"; //跳转至主页面
+                            alert('登录成功！')
+                        }
+                        else{console.log("未获取到token，登陆失败！")}
+                    }
+                    else{
+                        alert('登录失败，请重试！')
+                    }    
+                })
+                .catch(err=>showOutput(err));
         }
         else if(!needEmail.test(email)){
             alert("请输入正确格式的邮件！")
@@ -40,8 +53,4 @@ document.onkeydown = function (e) { // 回车提交表单
         if (code == 13) {
             check();
         }
-    }
-    function signUpPage(){
-        //跳转至注册页面
-        //需完善
-    }
+}
